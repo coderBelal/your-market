098fc902e99831e34b549afda82a804778471acb
Belal Uddin <coderbelal99@gmail.com>
2024-12-04 07:42:59 +0600

    basic routing setup

---
+++ src/components/Footer.tsx
@@ -0,0 +1,7 @@
+ 
+
+const Footer = () => {
+  return <footer>&copy; 2024 Your Website</footer>;
+};
+
+export default Footer;
---
+++ src/router.tsx
@@ -0,0 +1,33 @@
+import { createBrowserRouter } from "react-router-dom";
+import Layout from "./components/Layout";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Contact from "./pages/Contact";
+import NotFound from "./pages/NotFound";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Layout />,
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
+      {
+        path: "/about",
+        element: <About />,
+      },
+      {
+        path: "/contact",
+        element: <Contact />,
+      },
+    ],
+  },
+  {
+    path: "*",
+    element: <NotFound />,
+  },
+]);
+
+export default router;
